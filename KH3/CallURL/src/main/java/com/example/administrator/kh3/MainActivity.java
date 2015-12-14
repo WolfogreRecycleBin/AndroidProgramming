a849b298c5f1586b7d08b081d41a616fe33c26fd
Jason Song <wolfogre@outlook.com>
2015-12-14 14:48:55 +0800

    Renamed KH2 to KH3

---
+++ KH3/CallURL/src/main/java/com/example/administrator/kh3/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.administrator.kh3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.net.Uri;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.baidu.com"));
+                try {
+                    startActivity(intent);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+}
