08df872c5a836ff8ccd8cec567d2c23131d43025
Jason Song <wolfogre@outlook.com>
2015-12-30 17:05:24 +0800

    Made it can feel touching

--- KHTD4/snake/src/main/java/wolfogre/khtd4/MainActivity.java
+++ KHTD4/snake/src/main/java/wolfogre/khtd4/MainActivity.java
@@ -2,21 +2,71 @@ package wolfogre.khtd4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
+	GestureDetector gestureDetector;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Button button = (Button) findViewById(R.id.button);
-		button.setOnClickListener(new View.OnClickListener() {
+
+		findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				((BoardView)findViewById(R.id.view)).restart();
+				((BoardView) findViewById(R.id.view)).restart();
+				Toast.makeText(MainActivity.this, "restart", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		View activity = findViewById(R.id.activity);
+		activity.setOnTouchListener(this);
+		activity.setFocusable(true);
+		activity.setClickable(true);
+		activity.setLongClickable(true);
+
+		gestureDetector = new GestureDetector( new GestureDetector.OnGestureListener() {
+			@Override
+			public boolean onDown(MotionEvent e) {
+				return false;
+			}
+
+			@Override
+			public void onShowPress(MotionEvent e) {
+
+			}
+
+			@Override
+			public boolean onSingleTapUp(MotionEvent e) {
+				return false;
+			}
+
+			@Override
+			public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+				return false;
+			}
+
+			@Override
+			public void onLongPress(MotionEvent e) {
+
+			}
+
+			@Override
+			public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+				Toast.makeText(MainActivity.this, "onFling " + velocityX + " " + velocityY, Toast.LENGTH_LONG).show();
+				return false;
 			}
 		});
 	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		return gestureDetector.onTouchEvent(event);
+	}
+
 }
