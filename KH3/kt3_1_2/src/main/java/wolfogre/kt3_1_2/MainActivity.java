f6e7d70b7e3dfacfe5cbea7612b1b142857da311
Jason Song <wolfogre@outlook.com>
2016-01-30 21:47:06 +0800

    Rename CallURL -> KT3_1_2

---
+++ KH3/kt3_1_2/src/main/java/wolfogre/kt3_1_2/MainActivity.java
@@ -0,0 +1,29 @@
+package wolfogre.kt3_1_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + ((EditText)findViewById(R.id.edit_text)).getText().toString()));
+				try {
+					startActivity(intent);
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+	}
+}
