ffe5b62a0cd021da0668d1ac59fb9d4d1a13c51a
Jason Song <wolfogre@outlook.com>
2016-02-09 00:01:58 +0800

    Items of ListView can not be clicked

---
+++ KH6/kh6_1/src/main/java/wolfogre/kh6_1/ListAdapter.java
@@ -0,0 +1,78 @@
+package wolfogre.kh6_1;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by Jason Song(wolfogre@outlook.com) on 02/08/2016.
+ */
+public class ListAdapter extends BaseAdapter {
+	Context context;
+	ArrayList<File> list;
+	LayoutInflater inflater;
+
+	public ListAdapter(ArrayList<File> list, Context context) {
+		super();
+		this.context = context;
+		this.list = list;
+		inflater = LayoutInflater.from(context);
+	}
+
+	@Override
+	public int getCount() {
+		return list.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return list.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ViewHolder holder;
+		if (convertView == null) {
+			holder = new ViewHolder();
+			convertView = inflater.inflate(R.layout.list_item, null);
+			holder.imageView = (ImageView) convertView.findViewById(R.id.imageView);
+			holder.textView = (TextView) convertView.findViewById(R.id.textView);
+			holder.button = (Button) convertView.findViewById(R.id.button);
+			convertView.setTag(holder);
+		} else {
+			holder = (ViewHolder) convertView.getTag();
+		}
+
+		if(list.get(position).isFile()){
+			holder.imageView.setImageResource(R.mipmap.ic_insert_drive_file_black_48dp);
+		}else{
+			holder.imageView.setImageResource(R.mipmap.ic_folder_open_black_48dp);
+		}
+		holder.textView.setText(list.get(position).getName());
+		//holder.button = (Button) convertView.findViewById(R.id.button);
+
+		return convertView;
+	}
+
+	private static class ViewHolder {
+		ImageView imageView ;
+		TextView textView;
+		Button button;
+	}
+}
--- KH6/kh6_1/src/main/java/wolfogre/kh6_1/MainActivity.java
+++ KH6/kh6_1/src/main/java/wolfogre/kh6_1/MainActivity.java
@@ -1,15 +1,23 @@
 package wolfogre.kh6_1;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+
+	String path;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,31 +30,41 @@ public class MainActivity extends AppCompatActivity {
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+				//TODO
+			}
+		});
+
+		path = Environment.getExternalStorageDirectory().getPath();
+
+		ArrayList<File> list =new ArrayList<>();
+		Collections.addAll(list, new File(path).listFiles());
+
+		ListView listView=(ListView)this.findViewById(R.id.list_view);
+		listView.setAdapter(new ListAdapter(list, this));
+		final Context context = this;
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Toast.makeText(context, "点击了" + position,Toast.LENGTH_LONG).show();
+			}
+		});
+
+		listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				Toast.makeText(context, "选择了" + position,Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+
 			}
 		});
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
+	protected void onStart() {
+		super.onStart();
+		new DescriptionDialogFragment().show(getFragmentManager(),"");
 	}
 }
