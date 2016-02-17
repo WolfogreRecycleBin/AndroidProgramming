4d986cbe892291fbb1d3db9b8f9abf07cc34863c
Jason Song <wolfogre@outlook.com>
2016-02-17 17:27:39 +0800

    New module kh6_2

---
+++ KH6/kh6_2/src/main/java/wolfogre/kh6_2/DescriptionDialogFragment.java
@@ -0,0 +1,30 @@
+package wolfogre.kh6_2;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by Jason Song(wolfogre@outlook.com) on 02/08/2016.
+ */
+public class DescriptionDialogFragment extends DialogFragment {
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		super.onCreateDialog(savedInstanceState);
+		// Use the Builder class for convenient dialog construction
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+		builder.setMessage(getString(R.string.app_description))
+				.setNegativeButton("确认", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.cancel();
+					}
+				})
+				.setTitle(getString(R.string.app_name));
+		// Create the AlertDialog object and return it
+		return builder.create();
+	}
+}
